2d623796092c96ed6555b22252a7cf567aba2de1
ManuelG-10 <108595854+ManuelG-10@users.noreply.github.com>
2023-08-07 20:11:02 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbwC_bPu72l7gjcfaSu0ggYRHm7eb9HczBsKXDq2pzTo61FFi7lS2Qq2zjXZvdcLs0msgQ/exec";
