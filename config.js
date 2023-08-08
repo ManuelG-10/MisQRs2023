bd9316bae6511b62eb1f023fb774a0031c1e3ee6
ManuelG-10 <108595854+ManuelG-10@users.noreply.github.com>
2023-08-07 20:18:21 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbxwxWwsjfujDpj0buDgaKa6Zx5oApTEdTaIhLP-c_uybu-srmJu2x8h6xeYWzbnTGbKBw/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbxHgMM3_lb7bfbJUHSknCsdt7pitm9Is82FH-AtUQsWxJGOOzsr1Q6jRIBCZWX29BUHBA/exec";
